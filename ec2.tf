7d4dacc38453ef3071b28e71fe9dbdf469fc7984
Zhou Rui <wirelesser@hotmail.com>
2022-05-14 10:09:45 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,43 +1,9 @@
 resource "aws_instance" "example" {
   ami                    = "ami-02a45d709a415958a"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              python3 -m http.server 8080 &
-              EOF
+  instance_type          = "t2.micro"  
 
   tags = {
     Name = "terraform-example"
   }
 }
 
-resource "aws_security_group" "instance" {
-
-  name = var.security_group_name
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-variable "security_group_name" {
-  description = "The name of the security group"
-  type        = string
-  default     = "terraform-example-instance"
-}
-
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the Instance"
-}
